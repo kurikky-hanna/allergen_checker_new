38b76f1947f8cee66a92c681007d13c82fcdd782
kurikky-hanna <rikukimura0603@gmail.com>
2026-09-10 09:13:18 +0900

    Change title display to write method for PDF upload

--- Contest_Prezentation_V1.py
+++ Contest_Prezentation_V1.py
@@ -268,7 +268,7 @@ st.set_page_config(
 )
 
 st.title("給食アレルゲン調査機🍔")
-st.title("学校から配布された「食物アレルギー原因食品一覧表」のPDFをアップロードしてください。")
+st.write("学校から配布された「食物アレルギー原因食品一覧表」のPDFをアップロードしてください。")
 uploaded = st.file_uploader(
     " ",
     type=["pdf"],
